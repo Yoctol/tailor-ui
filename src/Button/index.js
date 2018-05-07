8a59034ee95f8a1840e6ba5f7ac10e23c25d0dad
jigsawye <jigsaw.ye@gmail.com>
2018-05-07 15:23:42 +0800

    Add simple Button

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { space, width } from 'styled-system';
 
 const Button = styled.button`
   width: 160px;
-  padding: 2px 3px;
+  height: 40px;
+  background-color: #fff;
+  border-radius: 4px;
+  border: solid 1px #6f7276;
+  ${space} ${width};
 `;
 
 export default Button;
