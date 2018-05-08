a90297f0af318968bcdbc17f9a6c89747791be4f
jigsawye <jigsaw.ye@gmail.com>
2018-05-08 17:28:26 +0800

    Updae colors and add spacing

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,22 +1,22 @@
 import { darken, lighten } from 'polished';
 
-const grayLevels = [0.03, 0.05, 0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.95];
+const grayLevels = [0.05, 0.1, 0.15, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9];
 
-export const primary = '#0E0C1A';
-export const primaryDark = darken(0.03, primary);
-export const primaryLight = lighten(0.03, primary);
+export const primary = '#2f2a4a';
+export const primaryDark = '#0e0c1a';
+export const primaryLight = '#423b63';
 
-export const secondary = '#80BDC7';
-export const secondaryDark = darken(0.03, secondary);
-export const secondaryLight = lighten(0.03, secondary);
+export const secondary = '#80bdc7';
+export const secondaryDark = '#59a6b3';
+export const secondaryLight = '#9fdfea';
 
-export const dark = '#303030';
+export const dark = '#111';
 export const light = '#fff';
 export const gray = grayLevels.map(level => lighten(level, dark));
 
-export const success = '#63BF2D';
-export const warning = '#DE8A15';
-export const error = '#DA3333';
+export const success = '#63bf2d';
+export const warning = '#de8a15';
+export const error = '#da3333';
 
 export const border = lighten(0.6, dark);
 export const borderDark = darken(0.1, border);
