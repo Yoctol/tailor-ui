2f0ef24a2687b2f76053b39a9cb9cde25221dcf2
xxhomey19 <xxhomey19@gmail.com>
2018-05-08 18:06:11 +0800

    Add Dropdown stories

--- src/DropDown/__stories__/Dropdown.story.js
+++ src/DropDown/__stories__/Dropdown.story.js
@@ -7,11 +7,28 @@ import Dropdown from '../';
 
 storiesOf('Dropdown', module)
   .addDecorator(centered)
-  .add('Active', () => (
-    <Dropdown active style={{ position: 'relative' }}>
+  .add('with visible', () => (
+    <Dropdown visible style={{ position: 'relative' }}>
       <Dropdown.List>
         <Dropdown.Item onClick={action('Item 1 Clicked')}>Item 1</Dropdown.Item>
         <Dropdown.Item onClick={action('Item 2 Clicked')}>Item 2</Dropdown.Item>
+      </Dropdown.List>
+    </Dropdown>
+  ))
+  .add('with fancy colors', () => (
+    <Dropdown visible style={{ position: 'relative' }}>
+      <Dropdown.List>
+        {['99b898', 'feceab', 'ff847c', 'e84a5f', '2a363b'].map(color => (
+          <Dropdown.Item
+            p={3}
+            bg={`#${color}`}
+            color="#fff"
+            border="1px solid #fff"
+            onClick={action(`#${color} Clicked`)}
+          >
+            {color}
+          </Dropdown.Item>
+        ))}
       </Dropdown.List>
     </Dropdown>
   ));
--- src/DropDown/index.js
+++ src/DropDown/index.js
@@ -1,10 +1,10 @@
-import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import {
   themeGet,
   space,
   color,
-  border,
-  borderRadius,
+  borders,
   textAlign,
   width,
   top,
@@ -12,31 +12,29 @@ import {
   fontSize,
 } from 'styled-system';
 import { ifProp } from 'styled-tools';
+import { darken } from 'polished';
 
-import defaultTheme from '../theme';
-import { controlShadow } from '../utils/shadow';
+import theme from '../theme';
 
 const Dropdown = styled.div`
   position: absolute;
   z-index: 99;
-
-  ${controlShadow(themeGet('colors.primary'))};
-
-  ${ifProp(
-    'visible',
-    css`
-      display: 'block';
-    `,
-    css`
-      display: 'none';
-    `
-  )};
+  display: ${ifProp('visible', 'block', 'none')};
+  box-shadow: 0 0.1rem 0.2rem 0 rgba(0, 0, 0, 0.2);
 
   ${width} ${top} ${right}
 `;
 
+Dropdown.propTypes = {
+  visible: PropTypes.bool,
+  ...width.propTypes,
+  ...top.propTypes,
+  ...right.propTypes,
+};
+
 Dropdown.defaultProps = {
-  theme: defaultTheme,
+  theme,
+  visible: false,
   top: 36,
   right: 0,
   width: 165,
@@ -44,16 +42,23 @@ Dropdown.defaultProps = {
 
 const List = styled.ul`
   list-style: none;
+  background-color: transparent;
 
   :focus {
     outline: 0;
   }
 
-  ${color} ${space} ${border} ${borderRadius} ${textAlign};
+  ${space} ${borders} ${textAlign};
 `;
 
+Dropdown.propTypes = {
+  ...space.propTypes,
+  ...borders.propTypes,
+  ...textAlign.propTypes,
+};
+
 List.defaultProps = {
-  theme: defaultTheme,
+  theme,
   m: 0,
   p: 0,
   textAlign: 'center',
@@ -62,27 +67,35 @@ List.defaultProps = {
 Dropdown.List = List;
 
 const Item = styled.li`
+  padding: ${themeGet('space.2')} ${themeGet('space.0')};
   border: ${themeGet('borders.default')} ${themeGet('colors.borderDark')};
   cursor: pointer;
+  transition: all 0.2s ease-in-out;
 
   :hover {
-    background: #eee;
+    background-color: ${props =>
+      darken(0.05, themeGet(`colors.${props.bg}`, props.bg)(props))}
   }
 
   :not(:first-child) {
     border-top: 0;
   }
 
-  ${color} ${space} ${fontSize}
+  ${color} ${space} ${borders} ${fontSize}
 `;
 
+Dropdown.propTypes = {
+  ...color.propTypes,
+  ...space.propTypes,
+  ...borders.propTypes,
+  ...fontSize.propTypes,
+};
+
 Item.defaultProps = {
-  theme: defaultTheme,
+  theme,
   bg: 'light',
-  color: 'bodyFont',
+  color: 'gray.4',
   fontSize: 'default',
-  py: 2,
-  px: 0,
 };
 
 Dropdown.Item = Item;
