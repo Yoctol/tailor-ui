ad49d36f5d5616e2a61933db38e77796cebde9f2
jigsawye <jigsaw.ye@gmail.com>
2018-05-08 14:50:16 +0800

    New colors & theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,54 +1,65 @@
-// breakpoint values
-// any array length works with styled-system
+import * as colors from './colors';
+
+const lineHeight = '1rem';
+
 const breakpoints = ['40em', '52em', '64em'];
 
-const colors = {
-  blue: '#747d8c',
-  white: '#f2f2f2',
-  black: '#0c0d19',
-  gray: ['#6f7276', '#f5f5f5', '#d9d9d9', '#e0e0e0', '#5d5d5d'],
+const space = {
+  o: '.05rem',
+  h: '.1rem',
+  1: '.2rem',
+  2: '.4rem',
+  3: '.6rem',
+  4: '.8rem',
+  5: '1rem',
+  6: '1.2rem',
+  7: '1.4rem',
+  8: '1.6rem',
+  9: '1.8rem',
+  10: '2rem',
+  12: '2.4rem',
+  16: '3.2rem',
 };
 
-// space is used for margin and padding scales
-// it's recommended to use powers of two to ensure alignment
-// when used in nested elements
-// numbers are converted to px
-const space = [0, 4, 8, 16, 32, 64, 128, 256, 512];
-
-// typographic scale
-const fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 96, 128];
-
-// for any scale, either array or objects will work
-const lineHeights = [1, 1.125, 1.25, 1.5];
-
-const fontWeights = {
-  normal: 500,
-  bold: 700,
-};
-
-const letterSpacings = {
-  normal: 'normal',
-  caps: '0.25em',
+const fontSizes = {
+  default: '.8rem',
+  sm: '.7rem',
+  lg: '.9rem',
 };
 
 // border-radius
-const radii = [0, 2, 4, 8];
+const radii = [space[1]];
 
-const borders = [0, '1px solid', '2px solid', '4px solid'];
+const borders = {
+  none: '',
+  default: `${space.o} solid`,
+  lg: `${space.h} solid`,
+};
 
-const shadows = [`0 1px 2px 0 ${colors.text}`, `0 1px 4px 0 ${colors.text}`];
+const size = space[9];
+const sizeSm = space[7];
+const sizeLg = space[10];
+
+const controls = {
+  size,
+  sizeSm,
+  sizeLg,
+  paddingX: space[2],
+  paddingXSm: space[2] * 0.75,
+  paddingXLg: space[2] * 1.5,
+  paddingY: (size - lineHeight) / 2 - borders.default,
+  paddingYSm: (sizeSm - lineHeight) / 2 - borders.default,
+  paddingYLg: (sizeLg - lineHeight) / 2 - borders.default,
+};
 
 const theme = {
   breakpoints,
   colors,
   space,
   fontSizes,
-  lineHeights,
-  fontWeights,
-  letterSpacings,
   radii,
   borders,
-  shadows,
+  controls,
   borderColor: colors,
 };
 
