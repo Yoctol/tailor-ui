f89c96fe0b3f2cfccb953a66cb2d6174e52cf3ff
jigsawye <jigsaw.ye@gmail.com>
2018-05-09 10:44:00 +0800

    Add Card

---
+++ src/Card/index.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { themeGet, color, space, borderRadius } from 'styled-system';
+
+import theme from '../theme';
+
+const Card = styled.div`
+  border: ${themeGet('borders.default')} ${themeGet('colors.border')};
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+
+  ${color} ${borderRadius};
+`;
+
+Card.defaultProps = {
+  theme,
+  bg: 'bgLight',
+  borderRadius: 1,
+};
+
+const CardBlock = styled.div`
+  overflow: hidden;
+  border-bottom: ${themeGet('borders.default')} ${themeGet('colors.border')};
+
+  :last-child {
+    border-bottom: 0;
+  }
+
+  ${space};
+`;
+
+CardBlock.defaultProps = {
+  theme,
+  p: 4,
+};
+
+const CardImage = styled.div`
+  overflow: hidden;
+
+  :first-child {
+    padding-top: 0;
+
+    img {
+      border-top-left-radius: ${themeGet('radii.0')};
+      border-top-right-radius: ${themeGet('radii.0')};
+    }
+  }
+
+  :last-child {
+    img {
+      border-bottom-left-radius: ${themeGet('radii.0')};
+      border-bottom-right-radius: ${themeGet('radii.0')};
+    }
+  }
+
+  {space}
+`;
+
+CardImage.defaultProps = {
+  theme,
+  py: 4,
+};
+
+Card.CardBlock = CardBlock;
+Card.CardImage = CardImage;
+
+export default Card;
