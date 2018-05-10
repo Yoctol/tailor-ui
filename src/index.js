d08644cf212e9d36a79abe02e444f01b10acc38b
jigsawye <jigsaw.ye@gmail.com>
2018-05-10 17:10:16 +0800

    Add main

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as DropDown } from './DropDown';
+export { default as injectGlobalCss } from './injectGlobalCss';
+export { default as Menu } from './Menu';
+export { default as Modal } from './Modal';
+export { default as Tabs } from './Tabs';
+export { default as Tooltip } from './Tooltip';
