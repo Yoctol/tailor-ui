77c10d20220dda2affe81809bd44f37bca9ff43e
jigsawye <jigsaw.ye@gmail.com>
2018-05-10 14:34:27 +0800

    Add toggle to Dropdown story

--- src/DropDown/__stories__/Dropdown.story.js
+++ src/DropDown/__stories__/Dropdown.story.js
@@ -2,38 +2,62 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { action } from '@storybook/addon-actions';
+import { compose, withState, withHandlers } from 'recompose';
 
+import Button from '../../Button';
 import Dropdown from '../';
+
+const visibleEnhancer = compose(
+  withState('visible', 'setVisible', false),
+  withHandlers({
+    toggleVisible: ({ setVisible }) => () => setVisible(visible => !visible),
+  })
+);
+
+const WithToggle = visibleEnhancer(({ visible, toggleVisible, children }) => (
+  <div style={{ position: 'relative' }}>
+    {children(visible)}
+    <Button onClick={toggleVisible}>Toggle</Button>
+  </div>
+));
 
 storiesOf('Dropdown', module)
   .addDecorator(centered)
   .add('with visible', () => (
-    <Dropdown visible style={{ position: 'relative' }}>
-      <Dropdown.List>
-        <Dropdown.Item key="Item 1" onClick={action('Item 1 Clicked')}>
-          Item 1
-        </Dropdown.Item>
-        <Dropdown.Item key="Item 2" onClick={action('Item 2 Clicked')}>
-          Item 2
-        </Dropdown.Item>
-      </Dropdown.List>
-    </Dropdown>
+    <WithToggle>
+      {visible => (
+        <Dropdown top={37} visible={visible}>
+          <Dropdown.List>
+            <Dropdown.Item key="Item 1" onClick={action('Item 1 Clicked')}>
+              Item 1
+            </Dropdown.Item>
+            <Dropdown.Item key="Item 2" onClick={action('Item 2 Clicked')}>
+              Item 2
+            </Dropdown.Item>
+          </Dropdown.List>
+        </Dropdown>
+      )}
+    </WithToggle>
   ))
   .add('with fancy colors', () => (
-    <Dropdown visible style={{ position: 'relative' }}>
-      <Dropdown.List>
-        {['99b898', 'feceab', 'ff847c', 'e84a5f', '2a363b'].map(color => (
-          <Dropdown.Item
-            key={color}
-            p={3}
-            bg={`#${color}`}
-            color="#fff"
-            border="1px solid #fff"
-            onClick={action(`#${color} Clicked`)}
-          >
-            {color}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.List>
-    </Dropdown>
+    <WithToggle>
+      {visible => (
+        <Dropdown top={37} visible={visible}>
+          <Dropdown.List>
+            {['99b898', 'feceab', 'ff847c', 'e84a5f', '2a363b'].map(color => (
+              <Dropdown.Item
+                key={color}
+                p={3}
+                bg={`#${color}`}
+                color="#fff"
+                border="1px solid #fff"
+                onClick={action(`#${color} Clicked`)}
+              >
+                {color}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.List>
+        </Dropdown>
+      )}
+    </WithToggle>
   ));
