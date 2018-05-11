4c8581a2fb42b21fc34bc85ae009552a7dbd6199
jigsawye <jigsaw.ye@gmail.com>
2018-05-11 18:46:34 +0800

    Implement New menu UI

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,9 +1,11 @@
-import styled, { css } from 'styled-components';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Flex } from 'grid-styled';
 import { themeGet, space, color, borders, borderRadius } from 'styled-system';
 import { ifProp } from 'styled-tools';
 
-import defaultTheme from '../theme';
+import theme from '../theme';
 
 const Menu = styled(Flex)`
   overflow-y: auto;
@@ -11,6 +13,7 @@ const Menu = styled(Flex)`
   height: 100%;
   flex-direction: column;
   justify-content: start;
+
   ${color};
 `;
 
@@ -19,31 +22,25 @@ Menu.propTypes = {
 };
 
 Menu.defaultProps = {
-  theme: defaultTheme,
-  bg: 'gray.4',
+  theme,
+  bg: 'primaryDark',
 };
 
 const Item = styled.button`
-  padding: 15px 30px;
+  width: 100%;
+  padding: 15px 16px;
+  border: 0;
+  border-left: ${themeGet('borders.xl')}
+    ${ifProp('active', themeGet('colors.secondary'), 'transparent')};
   flex-shrink: 0;
+  color: ${themeGet('colors.light')};
   text-align: left;
+  transition: all 0.1s ease-in-out;
   cursor: pointer;
-  ${ifProp(
-    'active',
-    css`
-      color: ${themeGet('colors.bodyFont')};
-      background-color: ${themeGet('colors.light')};
-    `,
-    css`
-      color: ${themeGet('colors.light')};
-      background-color: ${themeGet('colors.gray.4')};
-    `
-  )} &:hover {
-    background-color: ${ifProp(
-      'active',
-      themeGet('colors.gray.7'),
-      themeGet('colors.gray.3')
-    )};
+  background-color: ${themeGet('colors.primaryDark')};
+
+  &:hover {
+    opacity: 1;
   }
 
   &:focus {
@@ -53,19 +50,95 @@ const Item = styled.button`
   ${space};
   ${borders};
   ${borderRadius};
+
+  opacity: ${ifProp('active', 1, 0.6)};
 `;
 
 Item.propTypes = {
   ...space.propTypes,
+  ...color.propTypes,
   ...borders.propTypes,
   ...borderRadius.propTypes,
 };
 
 Item.defaultProps = {
-  theme: defaultTheme,
-  border: 0,
+  theme,
 };
 
+const SubMenuWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  /* stylelint-disable no-duplicate-selectors */
+  ${Item} {
+    background-color: ${themeGet('colors.primaryDark')};
+  }
+  /* stylelint-enable */
+`;
+
+SubMenuWrapper.defaultProps = {
+  theme,
+};
+
+const Arrow = styled.div`
+  position: absolute;
+  z-index: 99;
+  top: 45px;
+  left: 50%;
+  width: 0;
+  height: 0;
+  margin-left: -2px;
+  border-top: 8px solid ${themeGet('colors.primaryDark')};
+  border-right: 7px solid transparent;
+  border-left: 7px solid transparent;
+`;
+
+Arrow.defaultProps = {
+  theme,
+};
+
+const SubMenuAnimation = styled.div`
+  overflow: hidden;
+  max-height: ${ifProp('active', '200px', 0)};
+  transition: all 0.3s ease-in-out;
+
+  /* stylelint-disable no-duplicate-selectors */
+  ${Item} {
+    background-color: ${themeGet('colors.primary')};
+
+    &:last-child {
+      border-bottom: ${themeGet('borders.default')} ${themeGet('colors.light')};
+    }
+  }
+  /* stylelint-enable */
+`;
+
+SubMenuAnimation.propTypes = {
+  active: PropTypes.bool.isRequired,
+};
+
+SubMenuAnimation.defaultProps = {
+  theme,
+};
+
+const SubMenu = ({ title, children, active, ...otherProps }) => (
+  <SubMenuWrapper>
+    <Item active={active} {...otherProps}>
+      {title}
+    </Item>
+    <Arrow />
+    <SubMenuAnimation active={active}>{children}</SubMenuAnimation>
+  </SubMenuWrapper>
+);
+
+SubMenu.propTypes = {
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
+};
+
+Menu.SubMenu = SubMenu;
 Menu.Item = Item;
 
 export default Menu;
