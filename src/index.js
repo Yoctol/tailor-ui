cacdedfa9bc257b843b1114bd109d8fe3c9bc174
jigsawye <jigsaw.ye@gmail.com>
2018-05-15 18:40:06 +0800

    Fix export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import * as colors from './theme/colors';
+
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Checkbox } from './Checkbox';
 export { default as Dropdown } from './Dropdown';
+export { default as Input } from './Form/Input';
+export { default as Label } from './Form/Label';
+export { default as Icon } from './Icon';
 export { default as injectGlobalCss } from './injectGlobalCss';
 export { default as Menu } from './Menu';
 export { default as Modal } from './Modal';
+export { default as Table } from './Table';
 export { default as Tabs } from './Tabs';
 export { default as Tooltip } from './Tooltip';
-export { default as Input } from './Form/Input';
-export { default as Label } from './Form/Label';
+export { colors };
