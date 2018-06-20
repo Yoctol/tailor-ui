df46bf3611b101c68ea393d339c326ecf32a1757
jigsawye <jigsaw.ye@gmail.com>
2018-06-20 15:03:00 +0800

    Add Checkbox & Radio docs

---
+++ src/Checkbox/README.md
@@ -0,0 +1,18 @@
+# Button Docs
+
+```js
+import { Checkbox, Label } from '@yoctol/ui';
+```
+
+### Try it with `knobs`:
+
+<!-- STORY -->
+
+## API
+
+This component needs using with `Label`.
+
+| Props      | type                                                                              | Default | Description          |
+| ---------- | --------------------------------------------------------------------------------- | ------- | -------------------- |
+| `disabled` | Boolean                                                                           | `false` |                      |
+| Color      | [View on docs](https://github.com/jxnblk/styled-system/blob/master/docs/table.md) |         | From `styled-stsyem` |
--- src/Radio/__stories__/Radio.story.js
+++ src/Radio/__stories__/Radio.story.js
@@ -1,57 +1,88 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import centered from '@storybook/addon-centered';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 
 import themeProvider from '../../../.storybook/theme-provider';
-import Label from '../../Form/Label';
+import { color } from '../../../.storybook/knobs';
+import {
+  withComponentReadme,
+  withComponentShowcase,
+} from '../../../.storybook/withDocs';
+import { Showcase, ShowcasePage } from '../../../.storybook/showcase';
+import { Heading } from '../../';
+import README from '../README.md';
+import { Label } from '../../../';
 import Radio from '../';
 
 storiesOf('Data Entry|Radio', module)
-  .addDecorator(centered)
+  .addDecorator(withKnobs)
   .addDecorator(themeProvider)
-  .add('default', () => (
-    <>
-      <Radio name="radio" id="a" defaultChecked onChange={action('onChange')} />
-      <Label htmlFor="a">Radio A</Label>
-      <br />
-      <Radio name="radio" id="b" defaultChecked onChange={action('onChange')} />
-      <Label htmlFor="b">Radio B</Label>
-    </>
-  ))
-  .add('disabled', () => (
-    <>
-      <Radio
-        name="radio"
-        id="a"
-        checked
-        disabled
-        onChange={action('onChange')}
-      />
-      <Label htmlFor="a">Checked Disabled</Label>
-      <br />
-      <Radio name="radio" id="b" disabled onChange={action('onChange')} />
-      <Label htmlFor="b">Disabled</Label>
-    </>
-  ))
-  .add('with cutomized color', () => (
-    <>
-      <Radio
-        name="customized"
-        id="custom-a"
-        bg="#63bf2d"
-        borderColor="#423b63"
-        onChange={action('onChange')}
-      />
-      <Label htmlFor="custom-a">Customized Color Radio A</Label>
-      <br />
-      <Radio
-        name="customized"
-        id="custom-b"
-        bg="#63bf2d"
-        borderColor="#423b63"
-        onChange={action('onChange')}
-      />
-      <Label htmlFor="custom-b">Customized Color Radio B</Label>
-    </>
-  ));
+  .add(
+    'Docs',
+    withComponentReadme(README, () => (
+      <Showcase>
+        <Radio
+          name="radio"
+          id="a"
+          disabled={boolean('disabled', false, 'props')}
+          defaultChecked
+          {...color()}
+          onChange={action('onChange')}
+        />
+        <Label htmlFor="a">Radio A</Label>
+        <br />
+        <Radio
+          name="radio"
+          id="b"
+          disabled={boolean('disabled', false, 'props')}
+          defaultChecked
+          {...color()}
+          onChange={action('onChange')}
+        />
+        <Label htmlFor="b">Radio B</Label>
+      </Showcase>
+    ))
+  )
+  .add(
+    'Showcase',
+    withComponentShowcase(() => (
+      <ShowcasePage title="Radio">
+        <Heading.h3>Radio with disabled</Heading.h3>
+        <Showcase>
+          <Radio
+            name="radio"
+            id="a"
+            checked
+            disabled
+            onChange={action('onChange')}
+          />
+          <Label htmlFor="a">Checked Disabled</Label>
+          <br />
+          <Radio name="radio" id="b" disabled onChange={action('onChange')} />
+          <Label htmlFor="b">Disabled</Label>
+        </Showcase>
+
+        <Heading.h3>Radio with customized color</Heading.h3>
+        <Showcase>
+          <Radio
+            name="customized"
+            id="custom-a"
+            bg="#63bf2d"
+            borderColor="#423b63"
+            onChange={action('onChange')}
+          />
+          <Label htmlFor="custom-a">Customized Color Radio A</Label>
+          <br />
+          <Radio
+            name="customized"
+            id="custom-b"
+            bg="#63bf2d"
+            borderColor="#423b63"
+            onChange={action('onChange')}
+          />
+          <Label htmlFor="custom-b">Customized Color Radio B</Label>
+        </Showcase>
+      </ShowcasePage>
+    ))
+  );
