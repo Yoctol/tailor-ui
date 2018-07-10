f8d5fbefc2cae4baada7763fceebe0af4b6aa4e4
terry623 <terry92516@gmail.com>
2018-07-10 15:11:43 +0800

    Add RadioField

---
+++ src/Form/RadioField.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Value } from 'react-powerplug';
+
+import Radio from '../Radio';
+import Space from '../Grid/Space';
+
+import Label from './Label';
+import Hint from './Hint';
+import FormField from './FormField';
+
+let seed = 0;
+
+const getUuid = () => {
+  seed += 1;
+  return `yoctol_radio_${Date.now()}_${seed}`;
+};
+
+const RadioField = ({
+  children,
+  success,
+  warning,
+  error,
+  message,
+  ...otherProps
+}) => {
+  const id = getUuid();
+  return (
+    <FormField success={success} warning={warning} error={error}>
+      <Radio id={id} {...otherProps} />
+      <Label htmlFor={id}>{children}</Label>
+      {(success || warning || error) && <Hint>{message}</Hint>}
+    </FormField>
+  );
+};
+
+RadioField.propTypes = {
+  children: PropTypes.string.isRequired,
+  error: PropTypes.bool,
+  message: PropTypes.string,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+};
+
+RadioField.defaultProps = {
+  error: false,
+  message: '',
+  success: false,
+  warning: false,
+};
+
+const RadioFieldGroup = ({
+  label: groupLabel,
+  values: groupValues,
+  options,
+  onChange,
+  success,
+  warning,
+  error,
+  message,
+  ...otherProps
+}) => (
+  <Value initial={groupValues} onChange={onChange}>
+    {({ value, set }) => (
+      <FormField success={success} warning={warning} error={error}>
+        <Label>{groupLabel}</Label>
+        {options.map(({ label, value: val, disabled = false }) => {
+          const id = getUuid();
+          return (
+            <Space mt={2} mb={1} key={label}>
+              <Radio
+                id={id}
+                disabled={disabled}
+                checked={value === val}
+                onChange={() => set(val)}
+                {...otherProps}
+              />
+              <Label htmlFor={id}>{label}</Label>
+            </Space>
+          );
+        })}
+        {(success || warning || error) && <Hint>{message}</Hint>}
+      </FormField>
+    )}
+  </Value>
+);
+
+RadioFieldGroup.propTypes = {
+  error: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      disabled: PropTypes.bool,
+    })
+  ).isRequired,
+  success: PropTypes.bool,
+  values: PropTypes.arrayOf(PropTypes.string).isRequired,
+  warning: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+};
+RadioFieldGroup.defaultProps = {
+  error: false,
+  message: '',
+  success: false,
+  warning: false,
+};
+
+RadioField.Group = RadioFieldGroup;
+
+export default RadioField;
---
+++ src/Form/__stories__/RadioField.story.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import centered from '@storybook/addon-centered';
+import { storiesOf } from '@storybook/react';
+import { Value } from 'react-powerplug';
+
+import themeProvider from '../../../.storybook/theme-provider';
+import RadioField from '../RadioField';
+import Box from '../../Grid/Box';
+
+// TODO: Wrong display order
+storiesOf('Data Entry|RadioField', module)
+  .addDecorator(centered)
+  .addDecorator(themeProvider)
+  .add('default', () => (
+    <Value initial="">
+      {({ set, value }) => (
+        <RadioField.Group
+          options={[
+            { label: 'Radio A', value: 'radio_A' },
+            { label: 'Radio B', value: 'radio_B' },
+          ]}
+          values={value}
+          onChange={val => set(val)}
+        />
+      )}
+    </Value>
+  ))
+  .add('group', () => (
+    <Value initial="">
+      {({ set, value }) => (
+        <RadioField.Group
+          label="Radio Group"
+          options={[
+            { label: 'Radio A', value: 'radio_A' },
+            { label: 'Radio B', value: 'radio_B' },
+            { label: 'Radio C', value: 'radio_C', disabled: true },
+            { label: 'Radio D', value: 'radio_D' },
+          ]}
+          values={value}
+          onChange={val => set(val)}
+        />
+      )}
+    </Value>
+  ))
+  .add('error', () => (
+    <Box width="200px">
+      <Value initial="">
+        {({ set, value }) => (
+          <RadioField.Group
+            label="Radio Group"
+            error
+            message="Something went wrong!"
+            options={[
+              { label: 'Radio A', value: 'radio_A' },
+              { label: 'Radio B', value: 'radio_B' },
+              { label: 'Radio C', value: 'radio_C' },
+              { label: 'Radio D', value: 'radio_D' },
+            ]}
+            values={value}
+            onChange={val => set(val)}
+          />
+        )}
+      </Value>
+    </Box>
+  ));
