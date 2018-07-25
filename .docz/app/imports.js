4458b023b438c0e75b73935c96b3b567a040a7f1
jigsawye <jigsaw.ye@gmail.com>
2018-07-25 14:42:03 +0800

    Remove .docz files

--- .docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'src/Button/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-button-index" */ 'src/Button/index.mdx'),
-  'src/Heading/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-heading-index" */ 'src/Heading/index.mdx'),
-  'src/Icon/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-icon-index" */ 'src/Icon/index.mdx'),
-}
