55b19ee3bb21643087d82f6983d5aa684fae46bc
jigsawye <jigsaw.ye@gmail.com>
2018-08-27 16:06:54 +0800

    style(alert): update animation & background-color

--- src/Alert/BaseAlert.js
+++ src/Alert/BaseAlert.js
@@ -2,7 +2,7 @@ import CloseIcon from 'react-icons/lib/md/close';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
-import { animated } from 'react-spring';
+import { rgba } from 'polished';
 import { space, themeGet } from 'styled-system';
 
 import Box from '../Grid/Box';
@@ -15,6 +15,8 @@ const StyledAlert = styled.div`
   border: ${themeGet('borders.default')};
   border-radius: ${themeGet('radii.1')};
   border-color: ${p => themeGet(`colors.${p.type}`)(p)};
+  background-color: ${p => rgba(themeGet(`colors.${p.type}`)(p), 0.1)};
+  font-size: ${themeGet('fontSizes.sm')};
   transform-origin: top;
 
   ${space};
@@ -26,37 +28,26 @@ StyledAlert.defaultProps = {
   mb: 4,
 };
 
-const AnimatedStyledAlert = animated(StyledAlert);
-
-const BaseAlert = ({ message, type, closable, styles, onClose, ...props }) => {
-  const RenderComponent = closable ? AnimatedStyledAlert : StyledAlert;
-  return (
-    <RenderComponent style={styles} type={type} {...props}>
-      <Box flex="none">{getTypeIcon(type)}</Box>
-      <Box flex="auto">{message}</Box>
-      {closable && (
-        <Box flex="none">
-          <Icon size="16" cursor="pointer" type={CloseIcon} onClick={onClose} />
-        </Box>
-      )}
-    </RenderComponent>
-  );
-};
+const BaseAlert = ({ message, type, closable, onClose, ...props }) => (
+  <StyledAlert type={type} {...props}>
+    <Box flex="none">{getTypeIcon(type)}</Box>
+    <Box flex="auto">{message}</Box>
+    {closable && (
+      <Box flex="none">
+        <Icon size="16" cursor="pointer" type={CloseIcon} onClick={onClose} />
+      </Box>
+    )}
+  </StyledAlert>
+);
 
 BaseAlert.propTypes = {
   closable: PropTypes.bool.isRequired,
   message: PropTypes.string.isRequired,
-  styles: PropTypes.shape({
-    transform: PropTypes.object,
-    opacity: PropTypes.object,
-    height: PropTypes.object,
-  }),
   type: PropTypes.string.isRequired,
   onClose: PropTypes.func,
 };
 
 BaseAlert.defaultProps = {
-  styles: {},
   onClose: () => {},
 };
 
