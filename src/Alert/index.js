c19b4c508aa00e5390d36ead3364024e3cc31d8a
jigsawye <jigsaw.ye@gmail.com>
2018-08-30 12:31:58 +0800

    feat(alert): add onClosed

--- src/Alert/index.js
+++ src/Alert/index.js
@@ -6,41 +6,40 @@ import { space } from 'styled-system';
 
 import BaseAlert from './BaseAlert';
 
+const AnimatedAlert = ({ onClosed, ...otherProps }) => (
+  <Toggle initial>
+    {({ on, set }) => (
+      <Spring
+        native
+        onRest={({ height }) => height === 0 && onClosed()}
+        from={{ transform: 'scale(1, 1)', opacity: 1, height: 'auto' }}
+        to={{
+          transform: on ? 'scale(1, 1)' : 'scale(1, 0)',
+          opacity: on ? 1 : 0,
+          height: on ? 'auto' : 0,
+        }}
+        config={{
+          ...config.default,
+          restSpeedThreshold: 1,
+          restDisplacementThreshold: 0.01,
+        }}
+        {...otherProps}
+      >
+        {({ transform, opacity, display, height, ...props }) => (
+          <animated.div style={{ transform, opacity, display, height }}>
+            <BaseAlert onClose={() => set(false)} {...props} />
+          </animated.div>
+        )}
+      </Spring>
+    )}
+  </Toggle>
+);
+
 class Alert extends PureComponent {
-  renderClosableAlert = () => (
-    <Toggle initial>
-      {({ on, set }) => (
-        <Spring
-          native
-          from={{ transform: 'scale(1, 1)', opacity: 1, height: 'auto' }}
-          to={{
-            transform: on ? 'scale(1, 1)' : 'scale(1, 0)',
-            opacity: on ? 1 : 0,
-            height: on ? 'auto' : 0,
-          }}
-          config={{
-            ...config.default,
-            restSpeedThreshold: 1,
-            restDisplacementThreshold: 0.01,
-          }}
-          {...this.props}
-        >
-          {({ transform, opacity, display, height, ...props }) => (
-            <animated.div style={{ transform, opacity, display, height }}>
-              <BaseAlert onClose={() => set(false)} {...props} />
-            </animated.div>
-          )}
-        </Spring>
-      )}
-    </Toggle>
-  );
-
-  renderAlert = () => <BaseAlert {...this.props} />;
-
   render() {
-    return this.props.closable
-      ? this.renderClosableAlert()
-      : this.renderAlert();
+    const RenderComponent = this.props.closable ? AnimatedAlert : BaseAlert;
+
+    return <RenderComponent {...this.props} />;
   }
 }
 
@@ -57,11 +56,16 @@ Alert.propTypes = {
    * Type of Alert styles
    */
   type: PropTypes.oneOf(['info', 'success', 'warning', 'error']),
+  /**
+   * Callback executed when close animation is completed
+   */
+  onClosed: PropTypes.func,
   ...space.propTypes,
 };
 
 Alert.defaultProps = {
   closable: false,
+  onClosed: () => {},
   type: 'info',
 };
 
