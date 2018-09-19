1195e7acd5fe728041c67bf4e7896552edd30e4a
jigsawye <jigsaw.ye@gmail.com>
2018-09-19 16:33:50 +0800

    fix: build types

--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -4,7 +4,7 @@ import { render } from 'react-testing-library';
 
 import defaultTheme from '../src/theme';
 
-const customRender = (node, ...options) =>
+const customRender = (node: any, ...options: any[]) =>
   render(
     <ThemeProvider theme={defaultTheme}>{node}</ThemeProvider>,
     ...options
