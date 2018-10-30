5a6d4a51ca54eadf9a1aa87b438c21a9dc43e483
chentsulin <chentsulin@gmail.com>
2018-10-30 23:26:05 +0800

    test(input): put Textarea tests to separated file

--- src/Input/__tests__/Input.spec.tsx
+++ src/Input/__tests__/Input.spec.tsx
@@ -6,8 +6,6 @@ import Button from '../../Button';
 
 import Input from '..';
 
-import Textarea from '../Textarea';
-
 describe('Input', () => {
   it('should render correctly', () => {
     const { container } = render(<Input placeholder="placeholder" />);
@@ -43,6 +41,7 @@ describe('Input', () => {
 
   it('should call onChange when change input', () => {
     const onChange = jest.fn();
+
     const { container } = render(<Input onChange={onChange} />);
 
     const input = container.querySelector('input');
@@ -56,51 +55,34 @@ describe('Input', () => {
     expect(input.value).toBe('change input');
   });
 
-  // it('should auto select on input when pass autoSelect', () => {
-  //   const { container } = render(
-  //     <Input defaultValue="default value" autoSelect />
-  //   );
-
-  //   const input = container.querySelector('input');
-
-  //   const selection = window.getSelection();
-
-  //   expect(container.activeElement).toBe(input);
-  //   expect(selection).toBe('default value');
-  // });
-
-  // it('should call onPressEnter when press enter on input', () => {
-  //   const onPressEnter = jest.fn();
-  //   const { container } = render(<Input onPressEnter={onPressEnter} />);
-
-  //   const input = container.querySelector('input');
-
-  //   fireEvent.change(input, {
-  //     target: {
-  //       value: 'change input',
-  //     },
-  //   });
-
-  //   fireEvent.keyPress(input, {
-  //     key: 'Enter',
-  //   });
-
-  //   expect(onPressEnter).toBeCalledWith({ key: 'Enter' });
-  // });
-});
-
-describe('Textarea', () => {
-  it('should render correctly', () => {
-    const { container } = render(<Textarea placeholder="placeholder" />);
-
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it('should render correctly with minRows & maxRows', () => {
+  it('should auto select on input when pass autoSelect', () => {
     const { container } = render(
-      <Textarea id="demo" placeholder="placeholder" minRows={3} maxRows={6} />
+      <Input defaultValue="default value" autoSelect />
     );
 
-    expect(container.firstChild).toMatchSnapshot();
+    const input = container.querySelector('input');
+
+    expect(input).toHaveFocus();
+
+    // window.getSelection is not implemented in jsdom, so it's hard to test selection here
+    // - https://github.com/jsdom/jsdom/issues/317
+    // const selection = window.getSelection();
+    // expect(selection).toBe('default value');
+  });
+
+  it('should call onPressEnter when press enter on input', () => {
+    const onPressEnter = jest.fn();
+
+    const { container } = render(<Input onPressEnter={onPressEnter} />);
+
+    const input = container.querySelector('input');
+
+    fireEvent.keyPress(input, {
+      key: 'Enter',
+      keyCode: 13,
+      which: 13,
+    });
+
+    expect(onPressEnter).toBeCalled();
   });
 });
