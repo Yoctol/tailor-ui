c3dd2095b7114ddf04b35b4db0f2748adfee4154
jigsawye <jigsaw.ye@gmail.com>
2018-11-27 14:55:55 +0800

    fix(modal): useKeydown effect callback

--- src/utils/useKeydown.ts
+++ src/utils/useKeydown.ts
@@ -1,24 +1,27 @@
 import { useEffect } from 'react';
 
 export interface IKeydownProps {
-  onKeydown: () => void;
+  onKeydown?: () => void;
   keyCode: number;
 }
 
 export const ESC_KEY_CODE = 27;
 
 const useKeydown = ({ keyCode: targetKeyCode, onKeydown }: IKeydownProps) => {
-  const handleKeydown = ({ keyCode }: KeyboardEvent) => {
-    if (keyCode === targetKeyCode) {
-      onKeydown();
-    }
-  };
+  useEffect(
+    () => {
+      const handleKeydown = (event: KeyboardEvent) => {
+        if (event.keyCode === targetKeyCode && onKeydown) {
+          onKeydown();
+        }
+      };
 
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeydown);
+      window.addEventListener('keydown', handleKeydown);
 
-    return window.removeEventListener('keydown', handleKeydown);
-  });
+      return () => window.removeEventListener('keydown', handleKeydown);
+    },
+    [targetKeyCode, onKeydown]
+  );
 
   return null;
 };
