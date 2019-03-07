c90ea2f902d285fc8b15ea58d816c3f05c2df932
jigsawye <jigsaw.ye@gmail.com>
2019-03-07 18:34:47 +0800

    test(mention): add basic define test

--- packages/tailor-ui-lab/src/__tests__/index.spec.ts
+++ packages/tailor-ui-lab/src/__tests__/index.spec.ts
@@ -1,7 +1,8 @@
-import { Menu } from '..';
+import { Mention, Menu } from '..';
 
 describe('index', () => {
   it('should export all components', () => {
+    expect(Mention).toBeDefined();
     expect(Menu).toBeDefined();
   });
 });
