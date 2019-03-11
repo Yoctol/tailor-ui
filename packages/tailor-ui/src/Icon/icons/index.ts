8fd8b7e32ae7d794f649ee2315e72cc4bb4e312e
jigsawye <jigsaw.ye@gmail.com>
2019-03-11 16:31:19 +0800

    feat(icon): update built-in icons

--- packages/tailor-ui/src/Icon/icons/index.ts
+++ packages/tailor-ui/src/Icon/icons/index.ts
@@ -3,30 +3,36 @@ import kurator from './kurator';
 import line from './line';
 import loading from './loading';
 import messenger from './messenger';
+import robot from './robot';
 import settings from './settings';
 import tags from './tags';
 import touch from './touch';
+import tutorial from './tutorial';
 import understood from './understood';
 
 export type BuiltInIconKeys =
   | 'line'
   | 'messenger'
+  | 'robot'
   | 'understood'
   | 'kurator'
   | 'touch'
   | 'analytics'
   | 'tags'
+  | 'tutorial'
   | 'settings'
   | 'loading';
 
 export const icons = {
   line,
   messenger,
+  robot,
   understood,
   kurator,
   touch,
   analytics,
   tags,
   settings,
+  tutorial,
   loading,
 };
