252187af76839d8703fce3c020159d6a15817e21
jigsawye <jigsaw.ye@gmail.com>
2019-03-15 15:54:03 +0800

    style(heading): update font size

--- packages/tailor-ui/src/Heading/Heading.tsx
+++ packages/tailor-ui/src/Heading/Heading.tsx
@@ -1,43 +1,26 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { ColorProps, SpaceProps, color, space } from 'styled-system';
 import { rem } from 'polished';
 
 import tag from '../utils/CleanTag';
 
-export type HeadingBaseProps = SpaceProps &
-  ColorProps & {
-    grayHint?: boolean;
-    grayLight?: boolean;
-    white?: boolean;
-  };
+export type HeadingBaseProps = SpaceProps & ColorProps;
 
 const HeadingBase = styled(tag.p)<HeadingBaseProps>`
   margin: 0;
   color: ${p => p.theme.colors.gray700};
-  font-weight: 500;
+  font-weight: bold;
   line-height: 1.5;
-
-  ${({ grayLight }) =>
-    grayLight &&
-    css`
-      color: ${p => p.theme.colors.gray600};
-    `}
-
-  ${({ grayHint }) =>
-    grayHint &&
-    css`
-      color: ${p => p.theme.colors.gray500};
-    `}
-
-  ${({ white }) =>
-    white &&
-    css`
-      color: ${p => p.theme.colors.light};
-    `}
 
   ${space};
   ${color};
+`;
+
+const hero = styled((props: any) => <HeadingBase as={tag.h1} {...props} />)<
+  HeadingBaseProps
+>`
+  font-size: ${rem('32px')};
 `;
 
 const h1 = styled((props: any) => <HeadingBase as={tag.h1} {...props} />)<
@@ -55,28 +38,29 @@ const h2 = styled((props: any) => <HeadingBase as={tag.h2} {...props} />)<
 const h3 = styled((props: any) => <HeadingBase as={tag.h3} {...props} />)<
   HeadingBaseProps
 >`
-  font-size: ${rem('18px')};
+  font-size: ${rem('20px')};
 `;
 
 const h4 = styled((props: any) => <HeadingBase as={tag.h4} {...props} />)<
   HeadingBaseProps
 >`
-  font-size: ${rem('16px')};
+  font-size: ${rem('18px')};
 `;
 
 const h5 = styled((props: any) => <HeadingBase as={tag.h5} {...props} />)<
   HeadingBaseProps
 >`
-  font-size: ${rem('14px')};
+  font-size: ${rem('16px')};
 `;
 
 const h6 = styled((props: any) => <HeadingBase as={tag.h6} {...props} />)<
   HeadingBaseProps
 >`
-  font-size: ${rem('12px')};
+  font-size: ${rem('14px')};
 `;
 
 export default {
+  hero,
   h1,
   h2,
   h3,
