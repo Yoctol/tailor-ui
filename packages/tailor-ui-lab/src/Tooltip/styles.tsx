bd4c71bb80d324d553ba897ff01268cc01267d7b
jigsawye <jigsaw.ye@gmail.com>
2019-04-01 18:30:04 +0800

    refactor(tooltip): deprecate text align and update styles

--- packages/tailor-ui-lab/src/Tooltip/styles.tsx
+++ packages/tailor-ui-lab/src/Tooltip/styles.tsx
@@ -1,23 +1,10 @@
 import styled from 'styled-components';
-import {
-  BorderRadiusProps,
-  ColorProps,
-  SpaceProps,
-  TextAlignProps,
-  borderRadius,
-  color,
-  space,
-  textAlign,
-} from 'styled-system';
+import { SpaceProps, space } from 'styled-system';
 
-export type StyledTooltipProps = SpaceProps &
-  ColorProps &
-  BorderRadiusProps &
-  TextAlignProps;
+export type StyledTooltipProps = SpaceProps;
 
-export const StyledTooltip = styled.div<StyledTooltipProps>`
-  border: ${p => p.theme.borders.base};
-  border-color: ${p => p.theme.colors.primaryDark2};
+export const StyledTooltip: any = styled.div<StyledTooltipProps>`
+  border-radius: ${p => p.theme.radii.lg};
   opacity: 0.9;
   background-color: ${p => p.theme.colors.primaryDark2};
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
@@ -26,14 +13,8 @@ export const StyledTooltip = styled.div<StyledTooltipProps>`
   white-space: nowrap;
 
   ${space};
-  ${color};
-  ${borderRadius};
-  ${textAlign};
 `;
 
 StyledTooltip.defaultProps = {
-  py: 1,
-  px: 2,
-  textAlign: 'center',
-  borderRadius: 'base',
+  p: 2,
 };
