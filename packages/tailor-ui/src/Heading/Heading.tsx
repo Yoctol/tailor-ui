d15c6edb235ef26953ebf7e8e8203483635e6216
jigsawye <jigsaw.ye@gmail.com>
2019-05-16 18:52:49 +0800

    chore: bump deps

--- packages/tailor-ui/src/Heading/Heading.tsx
+++ packages/tailor-ui/src/Heading/Heading.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ColorProps, SpaceProps, color, space } from 'styled-system';
 import { rem } from 'polished';
 
 export type HeadingBaseProps = SpaceProps & ColorProps;
 
-const HeadingBase = styled.p<HeadingBaseProps>`
+const headingStyles = css`
   margin: 0;
   color: ${p => p.theme.colors.gray700};
   font-weight: bold;
@@ -15,46 +14,39 @@ const HeadingBase = styled.p<HeadingBaseProps>`
   ${color};
 `;
 
-const hero = styled((props: any) => <HeadingBase as="h1" {...props} />)<
-  HeadingBaseProps
->`
+const hero = styled.h1<HeadingBaseProps>`
   font-size: ${rem('32px')};
+  ${headingStyles};
 `;
 
-const h1 = styled((props: any) => <HeadingBase as="h1" {...props} />)<
-  HeadingBaseProps
->`
+const h1 = styled.h1<HeadingBaseProps>`
   font-size: ${rem('28px')};
+  ${headingStyles};
 `;
 
-const h2 = styled((props: any) => <HeadingBase as="h2" {...props} />)<
-  HeadingBaseProps
->`
+const h2 = styled.h2<HeadingBaseProps>`
   font-size: ${rem('24px')};
+  ${headingStyles};
 `;
 
-const h3 = styled((props: any) => <HeadingBase as="h3" {...props} />)<
-  HeadingBaseProps
->`
+const h3 = styled.h3<HeadingBaseProps>`
   font-size: ${rem('20px')};
+  ${headingStyles};
 `;
 
-const h4 = styled((props: any) => <HeadingBase as="h4" {...props} />)<
-  HeadingBaseProps
->`
+const h4 = styled.h4<HeadingBaseProps>`
   font-size: ${rem('18px')};
+  ${headingStyles};
 `;
 
-const h5 = styled((props: any) => <HeadingBase as="h5" {...props} />)<
-  HeadingBaseProps
->`
+const h5 = styled.h5<HeadingBaseProps>`
   font-size: ${rem('16px')};
+  ${headingStyles};
 `;
 
-const h6 = styled((props: any) => <HeadingBase as="h6" {...props} />)<
-  HeadingBaseProps
->`
+const h6 = styled.h6<HeadingBaseProps>`
   font-size: ${rem('14px')};
+  ${headingStyles};
 `;
 
 export default {
