b145d7b23fa889c087c19c90b544a1a131e4bc95
jigsawye <jigsaw.ye@gmail.com>
2019-05-24 14:55:08 +0800

    chore: bump deps

--- jest.config.js
+++ jest.config.js
@@ -10,9 +10,6 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
     '^test/(.*)': '<rootDir>/test/$1.tsx',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/test/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
   },
   setupFilesAfterEnv: [require.resolve('./test/jest.setup.ts')],
   resetModules: true,
