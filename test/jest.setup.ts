f045fb771d6560b768242c3201cd0e5ce7da8b6c
jigsawye <jigsaw.ye@gmail.com>
2019-05-30 19:10:47 +0800

    chore: bump deps

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -7,4 +7,4 @@ import 'jest-styled-components';
 import 'jest-dom/extend-expect';
 
 // this is basically: afterEach(cleanup)
-import 'react-testing-library/cleanup-after-each';
+import '@testing-library/react/cleanup-after-each';
